c5a6452af58043d8bd232326d2d5e0c70001ac07
Deepak Surya <ideepaksuryas@gmail.com>
2025-03-26 17:27:20 +0530

    feat: add basename for router for github pages config

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,15 +3,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/projects/:projectId",
+      element: <ProjectDetail />,
+    },
+  ],
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/projects/:projectId",
-    element: <ProjectDetail />,
-  },
-]);
+    basename: "/dss-site",
+  }
+);
+
+console.log("Router basename:", router.basename);
 
 export default router;
